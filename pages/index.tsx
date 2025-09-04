1dd43b73127a9f26249f3203af0b51ab112a91ab
Gixxeh <osgide@gmail.com>
2025-09-04 19:30:32 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Bright Step Tutoring</h1>
+      <p>Welcome to the new website!</p>
+      <p>This is a placeholder homepage. More features coming soon.</p>
+    </main>
+  );
+}
