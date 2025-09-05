e455f0fbd3509de83778c65e1ffee549a0a58fc4
Gixxeh <osgide@gmail.com>
2025-09-05 20:13:09 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
